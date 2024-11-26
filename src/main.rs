7f89f62e8b957c18bed3147297bfdc831a133e4c
Iamasink <iamasink@proton.me>
2024-11-26 20:05:50 +0000

    add spin_sleep for fps handling, add mouse dragging

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 use macroquad::prelude::*;
+use std::time::Duration;
 const MIN_SIZE: i32 = 1;
 const MAX_SIZE: i32 = 100;
 const GRID_SIZE: usize = 32;
 
-#[macroquad::main("BasicShapes")]
+#[macroquad::main("Window")]
 async fn main() {
+    // setup spin_sleep
+    let max_fps = 120;
+    let mut interval = spin_sleep_util::interval(Duration::from_secs(1) / max_fps);
+
+    // vars
     let mut x: i32 = 0;
     let mut y: i32 = 0;
     let mut grid: [[i32; GRID_SIZE]; GRID_SIZE] = [[0; GRID_SIZE]; GRID_SIZE];
+    let mut mousepos = (0, 0);
+    let mut oldmousepos = (0, 0);
 
     let mut k = 0;
     let mut l = 0;
+
     // prefil grid
     for i in grid {
         l = 0;
@@ -26,8 +35,11 @@ async fn main() {
     let mut click = false;
 
     let mut cell_size: i32 = 64;
+
     loop {
         clear_background(RED);
+
+        // handle inputs
 
         if is_key_down(KeyCode::D) {
             x += 1;
@@ -54,6 +66,46 @@ async fn main() {
             }
         }
 
+        // get mouse position
+
+        oldmousepos = mousepos;
+        mousepos = (mouse_position().0 as i32, mouse_position().1 as i32);
+
+        let mousex = mousepos.0;
+        let mousey = mousepos.1;
+        let mouse_tile_x =
+            (((mousex + x) as f32 / (cell_size) as f32).floor() as usize).clamp(0, GRID_SIZE - 1);
+        let mouse_tile_y =
+            (((mousey + y) as f32 / (cell_size) as f32).floor() as usize).clamp(0, GRID_SIZE - 1);
+
+        if !click & is_key_down(KeyCode::E) {
+            grid[mouse_tile_x][mouse_tile_y] = if grid[mouse_tile_x][mouse_tile_y] == 1 {
+                0
+            } else {
+                1
+            };
+            click = true;
+        }
+        if click & !is_key_down(KeyCode::E) {
+            click = false;
+        }
+        if is_mouse_button_down(MouseButton::Right) {
+            let dx = oldmousepos.0 - mousepos.0;
+            let dy = oldmousepos.1 - mousepos.1;
+            x += dx;
+            y += dy;
+        }
+
+        if is_mouse_button_pressed(MouseButton::Left) {
+            grid[mouse_tile_x][mouse_tile_y] = if grid[mouse_tile_x][mouse_tile_y] == 1 {
+                0
+            } else {
+                1
+            };
+        }
+
+        // draw grid
+
         // for each element (array) in grid - y / k
         // for each elemennt in that array - x / l??????????????
         // grain of salt cuz i just fiddled till it works
@@ -77,43 +129,14 @@ async fn main() {
             k = k + 1;
         }
 
-        // get mouse position
-        let mouse_position = mouse_position();
-        let mousex = mouse_position.0;
-        let mousey = mouse_position.1;
-        let mouse_tile_x = (((mousex + x as f32).floor() / (cell_size) as f32).floor() as usize)
-            .clamp(0, GRID_SIZE - 1);
-        let mouse_tile_y = (((mousey + y as f32).floor() / (cell_size) as f32).floor() as usize)
-            .clamp(0, GRID_SIZE - 1);
-
-        if !click & is_key_down(KeyCode::E) {
-            grid[mouse_tile_x][mouse_tile_y] = if grid[mouse_tile_x][mouse_tile_y] == 1 {
-                0
-            } else {
-                1
-            };
-            click = true;
-        }
-        if click & !is_key_down(KeyCode::E) {
-            click = false;
-        }
-
-        if is_mouse_button_pressed(MouseButton::Left) {
-            grid[mouse_tile_x][mouse_tile_y] = if grid[mouse_tile_x][mouse_tile_y] == 1 {
-                0
-            } else {
-                1
-            };
-        }
-
         // draw info text
         let fps = get_fps();
-        let ft = macroquad::time::get_frame_time();
+        let ft = get_frame_time();
         let frametime_ms = 1000.0 * ft;
         let str = format!("Pos: {x}, {y}");
         let str2 = format!("fps: {fps}");
         let str3 = format!("frametime: {frametime_ms:.2}ms");
-        let str4 = format!("mouse pos: {mouse_position:?}");
+        let str4 = format!("mouse pos: {mousex} {mousey}");
         let str5 = format!("mouse tile: {mouse_tile_x}, {mouse_tile_y}");
         draw_text(&str, 20.0, 20.0, 30.0, DARKGRAY);
         draw_text(&str2, 20.0, 40.0, 30.0, DARKGRAY);
@@ -121,16 +144,10 @@ async fn main() {
         draw_text(&str4, 20.0, 80.0, 30.0, DARKGRAY);
         draw_text(&str5, 20.0, 100.0, 30.0, DARKGRAY);
 
-        // handle frame rate
-
-        let minimum_frame_time = 1. / 60.; // 60 FPS
-        let mut time_to_sleep = 0.;
-        if ft < minimum_frame_time {
-            time_to_sleep = (minimum_frame_time - ft) * 1000.;
-            println!("Sleep for {}ms", time_to_sleep);
-        }
-
+        // next_frame().await;
+        // spin_sleep::sleep(Duration::from_millis(time_to_sleep as u64));
+        // tick the spin_sleep
         next_frame().await;
-        std::thread::sleep(std::time::Duration::from_millis(time_to_sleep as u64));
+        interval.tick();
     }
 }
